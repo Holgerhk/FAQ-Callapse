4b040592778f699f798499c65c594abc8fee9d17
Holger Krogh <holger.krogh@gmail.com>
2021-09-23 14:03:26 +0200

    html css done

---
+++ script.js
@@ -0,0 +1,3 @@
+// loop through icons // apply eventlisenter
+// select parantNode/Element and classList.toggle a class to the box
+// elect siblingNode/Element and classList.toggle a class to the p
